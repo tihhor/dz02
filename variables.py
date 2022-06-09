70e144446d0367210dfc2f168a65c3d1ad727a01
tihhor <tihhor@gmail.com>
2022-06-09 12:36:16 +0200

    Initial commit

---
+++ variables.py
@@ -0,0 +1,11 @@
+# Овощи
+veg_name = 'морковь'
+veg_sort = 1
+veg_underground = True
+veg_price = 50.5
+
+print(type(veg_name))
+print(type(veg_sort))
+print(type(veg_underground))
+print(type(veg_price))
+
